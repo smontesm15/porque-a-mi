79150a777adb0bb53d68ac1294b699de2544cda4
smontesm15 <s.montesm@uniandes.edu.co>
2026-03-02 16:46:37 +0000

    JAVA SCCRIPT

---
+++ script.js
@@ -0,0 +1,9 @@
+//console.log("hola");
+
+const varTitulo = document.getElementById("titulo");
+
+varTitulo.addEventListener("click", () => {
+    varTitulo.innerText="¡Hola cara de bolas!";
+    varTitulo.style.backgroundColor=" rgb(240, 199, 145)"
+
+})
