b489dda9ba99b7a38fca3ba1c5b62c6c40c18fa1
smontesm15 <s.montesm@uniandes.edu.co>
2026-03-09 15:46:45 +0000

    Agregar contexto

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 //console.log("hola");
 
 const varTitulo = document.getElementById("titulo");
+const contextoParrafo = document.getElementById("texto")
+
+const listaTexto =  ["Amorsin era un emoji normal y corriente", "hasta que un dia decidio enamorarse "," lo trataron muy mal ","por eso es pobre quedo como esta ahora","pero es no siempre fue asi"];
+
+let estadoInicial = true;
+let indice = 0;
+
+
 
 varTitulo.addEventListener("click", () => {
-    varTitulo.innerText="¡Hola cara de bolas!";
-    varTitulo.style.backgroundColor=" rgb(240, 199, 145)"
+    if(estadoInicial === true) {
+        varTitulo.innerText="¡Hola cara de bolas!";
+        varTitulo.style.backgroundColor=" transparent";
+        varTitulo.style.color= "red";
+    } else {
+        varTitulo.innerText="Amorsin";
+        varTitulo.style.backgroundColor=" transparent";
+        varTitulo.style.color= " rgb(226, 229, 156)";
+    }
+    estadoInicial = !estadoInicial;
 
+})
+
+listaTexto[listaTexto [0] ];
+contextoParrafo.addEventListener("click", () => {
+    if (indice < listaTexto.length){
+    contextoParrafo.innerText = listaTexto[indice];
+    indice = indice +1;
+    } else {
+    indice = 0; //si quiero que vuelva a empezar
+    contextoParrafo.innerText="FIN :) .";
+    }
+                                                                                                              
 })
